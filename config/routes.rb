fd6297d412aeddc96ab9e1e2200c56f30ad73ddc
Golam Morshed <golamdotmorshed@gmail.com>
2021-08-03 11:53:28 +0600

    installed devise, project module and task submodule

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: "projects#index"
+  resources :projects do
+    resources :tasks
+  end
+  namespace :project do
+    resources :tasks
+  end
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
